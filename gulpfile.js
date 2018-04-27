bca9b6f52adbbbafec8a4ba3429fceaa8b2e108c
Rodzman <designrsp@gmail.com>
2018-04-27 08:45:53 -0300

    responsive menu update

--- gulpfile.js
+++ gulpfile.js
@@ -12,22 +12,6 @@ gulp.task('w', function () {
   gulp.watch('./src/assets/styles/**/*', ['app-scss']);
 });
 
-// gulp.task('compileBootstrap', () =>
-//   gulp.src('./node_modules/bootstrap/scss/bootstrap.scss')
-//   .pipe(customizeBootstrap('./src/assets/styles/scss/*.scss'))
-//   .pipe(sass())
-//   .pipe(uglifycss())
-//   .pipe(gulp.dest('./src/assets/styles/css'))
-// );
-
-// task to bootstrap scss
-// gulp.task('bootstrap-scss', () =>
-//   gulp.src('node_modules/bootstrap/scss/**/*.scss')
-//   .pipe(sass().on('error', sass.logError))
-//   // .pipe(uglifycss())
-//   .pipe(gulp.dest('src/assets/styles/css'))
-// );
-
 // task to app scss
 gulp.task('app-scss', function() {
   return gulp.src(['node_modules/normalize.css/normalize.css', 'node_modules/bootstrap/scss/**/*.scss', 'src/assets/styles/scss/**/*.scss'])
@@ -38,7 +22,7 @@ gulp.task('app-scss', function() {
 });
 
 gulp.task('lib', function() {
-  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'src/assets/js/fontawesome-all.min.js'])
+  return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js', 'src/assets/js/fontawesome-all.min.js'])
   .pipe(concat('lib.js'))
   .pipe(gulp.dest('./src/assets/js'));
 });
--- server.js
+++ server.js
@@ -4,5 +4,3 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
-
-console.log('server ok na porta 8080');
